c92db5821c0d7b2c5a8c2f72e38cabc45c818202
Lee Crockford <lee@leecrockford.me>
2021-10-17 14:54:58 +1100

    Initial Commit

---
+++ Module2Lesson2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Module2Lesson2
+//
+//  Created by Lee Crockford on 17/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Module2Lesson2/Module2Lesson2App.swift
@@ -0,0 +1,17 @@
+//
+//  Module2Lesson2App.swift
+//  Module2Lesson2
+//
+//  Created by Lee Crockford on 17/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Module2Lesson2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
